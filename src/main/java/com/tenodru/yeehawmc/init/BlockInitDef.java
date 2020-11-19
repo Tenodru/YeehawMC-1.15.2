63152c1095175f00eb32428eee09fbd23d9f0696
Pyrosalsa <zmgordon99@gmail.com>
2020-11-18 23:11:34 -0600

    Worked on extra wood-based blocks -- WIP

--- src/main/java/com/tenodru/yeehawmc/init/BlockInitDef.java
+++ src/main/java/com/tenodru/yeehawmc/init/BlockInitDef.java
@@ -1,13 +1,18 @@
 package com.tenodru.yeehawmc.init;
 
 import com.tenodru.yeehawmc.YeehawMC;
+import com.tenodru.yeehawmc.objects.blocks.CedarButtonBlock;
+import com.tenodru.yeehawmc.objects.blocks.CedarPressurePlateBlock;
 import com.tenodru.yeehawmc.objects.blocks.CedarSaplingBlock;
 import com.tenodru.yeehawmc.world.feature.CedarTree;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.FenceBlock;
 import net.minecraft.block.LeavesBlock;
 import net.minecraft.block.LogBlock;
+import net.minecraft.block.PressurePlateBlock.Sensitivity;
+import net.minecraft.block.StairsBlock;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 import net.minecraftforge.fml.RegistryObject;
@@ -28,4 +33,16 @@ public class BlockInitDef {
 	
 	public static final RegistryObject<Block> CEDAR_SAPLING = BLOCKS.register("cedar_sapling", 
 			() -> new CedarSaplingBlock(() -> new CedarTree(), Block.Properties.from(Blocks.OAK_SAPLING)));
+	
+	public static final RegistryObject<Block> CEDAR_STAIRS = BLOCKS.register("cedar_stairs",
+			() -> new StairsBlock(() -> BlockInit.cedar_planks.getDefaultState(), Block.Properties.create(Material.WOOD)));
+	
+	public static final RegistryObject<Block> CEDAR_FENCE = BLOCKS.register("cedar_fence",
+			() -> new FenceBlock(Block.Properties.create(Material.WOOD)));
+	
+	public static final RegistryObject<Block> CEDAR_BUTTON = BLOCKS.register("cedar_button",
+			() -> new CedarButtonBlock(Block.Properties.create(Material.WOOD)));
+	
+	public static final RegistryObject<Block> CEDAR_PRESSURE_PLATE = BLOCKS.register("cedar_pressure_plate",
+			() -> new CedarPressurePlateBlock(Sensitivity.EVERYTHING, Block.Properties.create(Material.WOOD)));
 }
