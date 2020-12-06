5fe6087e1eeab88bd18017a0b77ac60efd098c83
Alex <alexkong07@gmail.com>
2020-12-06 15:45:21 -0600

    Added coarse dirt to terrain gen
    Hill Country surface is now made up of grass and coarse dirt.

--- src/main/java/com/tenodru/yeehawmc/init/BiomeInit.java
+++ src/main/java/com/tenodru/yeehawmc/init/BiomeInit.java
@@ -2,16 +2,20 @@ package com.tenodru.yeehawmc.init;
 
 import com.tenodru.yeehawmc.YeehawMC;
 import com.tenodru.yeehawmc.world.biomes.HillCountryBiome;
+import com.tenodru.yeehawmc.world.gen.builders.HillCountrySurfaceBuilder;
 
 import net.minecraft.block.Blocks;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biome.Category;
 import net.minecraft.world.biome.Biome.RainType;
+import net.minecraft.world.gen.surfacebuilders.ConfiguredSurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilders.ISurfaceBuilderConfig;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
 import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.common.BiomeDictionary.Type;
+import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -26,10 +30,13 @@ public class BiomeInit {
 					.temperature(0.7f)
 					.waterColor(0x3F76E4)
 					.waterFogColor(0x050533)
-					.surfaceBuilder(SurfaceBuilder.DEFAULT, new SurfaceBuilderConfig
-							(Blocks.GRASS_BLOCK.getDefaultState(), 
-									BlockInit.limestone.getDefaultState(), 
-									Blocks.DIRT.getDefaultState()))
+					.surfaceBuilder(
+							new ConfiguredSurfaceBuilder<SurfaceBuilderConfig>(
+									register("hill_country", 
+											new HillCountrySurfaceBuilder(SurfaceBuilderConfig::deserialize)), 
+									new SurfaceBuilderConfig (Blocks.GRASS_BLOCK.getDefaultState(), 
+											Blocks.DIRT.getDefaultState(), 
+											Blocks.DIRT.getDefaultState())))
 					.category(Category.PLAINS)
 					.downfall(0.3f)
 					.depth(0.125f)
@@ -42,6 +49,11 @@ public class BiomeInit {
 	private static void registerBiome(Biome biome, Type... types) {
 		BiomeDictionary.addTypes(biome, types);
 		BiomeManager.addSpawnBiome(biome);
+	}
+	
+	@SuppressWarnings("deprecation")
+	private static <C extends ISurfaceBuilderConfig, F extends SurfaceBuilder<C>> F register(String key, F builderIn) {
+		return (F) (Registry.<SurfaceBuilder<?>>register(Registry.SURFACE_BUILDER, key, builderIn));
 	}
 
 }
---
+++ src/main/java/com/tenodru/yeehawmc/world/gen/builders/HillCountrySurfaceBuilder.java
@@ -0,0 +1,33 @@
+package com.tenodru.yeehawmc.world.gen.builders;
+
+import java.util.Random;
+import java.util.function.Function;
+
+import com.mojang.datafixers.Dynamic;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.chunk.IChunk;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
+
+public class HillCountrySurfaceBuilder extends SurfaceBuilder<SurfaceBuilderConfig> {
+
+	public HillCountrySurfaceBuilder(Function<Dynamic<?>, ? extends SurfaceBuilderConfig> configFactory) {
+		super(configFactory);
+	}
+
+	public void buildSurface(Random random, IChunk chunkIn, Biome biomeIn, int x, int z, int startHeight, double noise,
+			BlockState defaultBlock, BlockState defaultFluid, int seaLevel, long seed, SurfaceBuilderConfig config) {
+		if (noise > 2.0D) {
+			SurfaceBuilder.DEFAULT.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock,
+					defaultFluid, seaLevel, seed, SurfaceBuilder.CORASE_DIRT_DIRT_GRAVEL_CONFIG);
+		}
+		else {
+			SurfaceBuilder.DEFAULT.buildSurface(random, chunkIn, biomeIn, x, z, startHeight, noise, defaultBlock,
+					defaultFluid, seaLevel, seed, SurfaceBuilder.GRASS_DIRT_GRAVEL_CONFIG);
+		}
+
+	}
+
+}
