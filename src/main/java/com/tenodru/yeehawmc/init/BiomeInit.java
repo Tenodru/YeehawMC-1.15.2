f38f70dd3992624e59557a74903604bc5f3488f5
Alex <alexkong07@gmail.com>
2020-12-06 23:37:36 -0600

    Hill Country now properly spawns with default worldgen

--- src/main/java/com/tenodru/yeehawmc/init/BiomeInit.java
+++ src/main/java/com/tenodru/yeehawmc/init/BiomeInit.java
@@ -16,6 +16,8 @@ import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.common.BiomeDictionary.Type;
 import net.minecraftforge.common.BiomeManager;
+import net.minecraftforge.common.BiomeManager.BiomeEntry;
+import net.minecraftforge.common.BiomeManager.BiomeType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -43,11 +45,13 @@ public class BiomeInit {
 					.parent(null)));
 	
 	public static void registerBiomes() {
-		registerBiome(HILL_COUNTRY_BIOME.get(), Type.OVERWORLD, Type.DRY, Type.PLAINS, Type.SAVANNA, Type.HILLS, Type.FOREST);
+		registerBiome(HILL_COUNTRY_BIOME.get(), BiomeType.WARM, 10, Type.OVERWORLD, Type.DRY, Type.PLAINS, Type.SAVANNA, Type.HILLS, Type.FOREST);
 	}
 	
-	private static void registerBiome(Biome biome, Type... types) {
+	private static void registerBiome(Biome biome, BiomeType biomeType, int weight, Type... types) {
+		ForgeRegistries.BIOMES.register(biome);
 		BiomeDictionary.addTypes(biome, types);
+		BiomeManager.addBiome(biomeType, new BiomeEntry(biome, weight));
 		BiomeManager.addSpawnBiome(biome);
 	}
 	
